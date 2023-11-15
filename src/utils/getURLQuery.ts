5be801d5ba2477dd1910aee6a13e9a3aca752594
jiwonMe <park@jiwon.me>
2023-11-15 22:36:48 +0900

    fix: hotfix

--- src/utils/getURLQuery.ts
+++ src/utils/getURLQuery.ts
@@ -1,4 +1,6 @@
 const getURLQuery = () => {
+  // ?memberSeq=123&sodaeVal=123&traineeName=123&traineeBirth=123
+
   // get url queries from url
   const url = new URL(window.location.href);
   const memberSeq = url.searchParams.get('memberSeq');
@@ -7,10 +9,10 @@ const getURLQuery = () => {
   const traineeBirth = url.searchParams.get('traineeBirth');
 
   return {
-    memberSeq,
-    sodaeVal,
-    traineeName,
-    traineeBirth,
+    memberSeq: memberSeq || import.meta.env.VITE_MEMBER_SEQ,
+    sodaeVal: sodaeVal || import.meta.env.VITE_SODAE_VAL,
+    traineeName: traineeName || import.meta.env.VITE_TRAINEE_NAME,
+    traineeBirth: traineeBirth || import.meta.env.VITE_TRAINEE_BIRTH,
   };
 };
 
